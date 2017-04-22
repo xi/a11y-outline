56abaecd15232a9ad6ac84a9fc4fe6b71f0d35c0
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-04-22 15:12:43 +0200

    use treeview

--- src/outline.js
+++ src/outline.js
@@ -11,51 +11,51 @@ var createDialog = function(selector) {
 	return dialog;
 };
 
-var createList = function(items) {
-	var ul = document.createElement('ul');
-	ul.style.margin = 0;
-	Array.prototype.forEach.call(items, function(item) {
-		var li = document.createElement('li');
-		li.appendChild(item);
-		ul.appendChild(li);
-	});
-	return ul;
+var createItem = function(el, i) {
+	var label = aria.getRole(el);
+	var name = aria.getName(el, null, true);
+	if (name) {
+		label = name + ' ' + label;
+	}
+
+	return {
+		label: label,
+		description: aria.getDescription(el),
+		href: '#' + i
+	};
 };
 
-var quickNav = function(selector, needsFocus) {
-	var matches = aria.querySelectorAll(document, selector)
+var buildTree = function(role, dialog, needsFocus) {
+	var matches = aria.querySelectorAll(document, role)
 		.filter(function(el) {
 			return !aria.matches(el, ':hidden');
 		});
-	var dialog = createDialog();
 
-	var links = Array.prototype.map.call(matches, function(el) {
-		var a = document.createElement('a');
-		a.href = '#';
-		a.addEventListener('click', function(event) {
+	var items = Array.prototype.map.call(matches, createItem);
+	var tree = treeview(items, role);
+
+	tree.addEventListener('click', function(event) {
+		if (event.target.matches('a')) {
 			event.preventDefault();
 			dialog.close();
+
+			var href = event.target.getAttribute('href');
+			var i = parseInt(href.substr(1), 10);
+			var el = matches[i];
+
 			if (needsFocus) {
 				el.tabIndex = -1;
 			}
 			el.focus();
-		});
-
-		var name = aria.getName(el, null, true);
-		if (name) {
-			a.textContent = name + ' ';
 		}
-
-		a.textContent += aria.getRole(el);
-
-		var description = aria.getDescription(el);
-		if (description && description != name) {
-			a.title = description;
-		}
-		return a;
 	});
 
-	dialog.appendChild(createList(links));
+	dialog.appendChild(tree);
+};
+
+var quickNav = function(selector, needsFocus) {
+	var dialog = createDialog();
+	buildTree(selector, dialog, needsFocus);
 	dialog.showModal();
 };
 
