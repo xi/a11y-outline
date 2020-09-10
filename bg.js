59aed7c7078590a4841519bb6a66cbb429f1719c
Tobias Bengfort <tobias.bengfort@posteo.de>
2020-09-10 10:41:20 +0200

    forward commands to content script

--- bg.js
+++ bg.js
@@ -1,3 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.sendMessage(tab.id, 'showA11yOutline');
 });
+
+chrome.commands.onCommand.addListener(function(command) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, command);
+	});
+});
