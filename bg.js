963c4585cf71ef984b10de2c745f1dad4a619167
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-07-17 23:21:16 +0200

    use activeTab instead of <all_urls> permission

--- bg.js
+++ bg.js
@@ -1,9 +1,35 @@
+/* global chrome */
+
+var wrap = function(fn) {
+	return function(...args) {
+		return new Promise((resolve) => {
+			fn(...args, result => {
+				resolve(result);
+			});
+		});
+	};
+};
+
+var executeScript = wrap(chrome.tabs.executeScript);
+
+var injectCode = function() {
+	return Promise.all([
+		executeScript({file: '/vendor/aria.js'}),
+		executeScript({file: '/treeview.js'}),
+		executeScript({file: '/outline.js'}),
+	]);
+};
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.sendMessage(tab.id, 'showA11yOutline');
+	injectCode().then(() => {
+		chrome.tabs.sendMessage(tab.id, 'showA11yOutline');
+	});
 });
 
 chrome.commands.onCommand.addListener(function(command) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, command);
+		injectCode().then(() => {
+			chrome.tabs.sendMessage(tabs[0].id, command);
+		});
 	});
 });
